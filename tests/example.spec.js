9b3ac0985b0c8095ddc318f7d69802bb557504f2
Noam Gaash <noam.gaash@applitools.com>
2024-04-24 14:19:31 +0300

    feat: MRE for worker fixture timeout

---
+++ tests/example.spec.js
@@ -0,0 +1,14 @@
+const { test } = require('@playwright/test');
+
+const myFixture = test.extend({
+  autoFixture: [async ({}, use) => {
+    await use();
+    console.log('Fixture teardown starts');
+    await new Promise((r) => setTimeout(r, 31_000));
+    console.log('Fixture teardown ends');
+  }, { auto: true, timeout: 60_000, scope: 'worker'}]
+});
+
+myFixture('my test', async ({}) => {
+  console.log('test');
+})
